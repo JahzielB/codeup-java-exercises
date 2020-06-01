d56c2111a22bcd1d74be96769e6a5fb4f544dfd6
JahzielB <jahzielb@gmail.com>
2020-06-01 15:37:23 -0500

    finished another exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+//        int myFavoriteNumber = 3;
+//        System.out.println(myFavoriteNumber);
+//        String myString = "This is a string.";
+//        System.out.println(myString);
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
     }
 }
