68f3a26dbcb88bc78b6a9faa8dc0a22935b27a36
JahzielB <jahzielb@gmail.com>
2020-06-03 11:18:17 -0500

    finished third question of control statements and loops exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import static java.lang.Math.pow;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
@@ -34,16 +37,33 @@ public class ControlFlowExercises {
 //            i *= i;
 //        }
 
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Fizz " + i);
-            }
-            if (i % 5 == 0) {
-                System.out.println("Buzz " + i);
-            }
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz " + i);
-            }
-        }
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0) {
+//                System.out.println("Fizz " + i);
+//            }
+//            if (i % 5 == 0) {
+//                System.out.println("Buzz " + i);
+//            }
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz " + i);
+//            }
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        String userInput;
+//        do {
+//            System.out.print("Enter an integer: ");
+//            long userInt = scanner.nextInt();
+//            System.out.printf("Here is your table!\n");
+//            System.out.printf("number | squared | cubed\n");
+//            System.out.printf("------ | ------- | -----\n");
+//            for (int i = 1; i <= userInt; i++) {
+//                System.out.printf("%-6d | %-7.0f | %-6.0f\n", i, Math.pow(i, 2), Math.pow(i, 3));
+//            }
+//            System.out.print("Would you like to continue?: ");
+//            userInput = scanner.next().toLowerCase();
+//        }while (userInput.equals("yes"));
+
+
     }
 }
