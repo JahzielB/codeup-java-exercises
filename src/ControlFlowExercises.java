c0c8637029a6efd44b2343b6914d55ddbd9f32b1
JahzielB <jahzielb@gmail.com>
2020-06-03 10:10:50 -0500

    completed second question of control statements and loops exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -34,6 +34,16 @@ public class ControlFlowExercises {
 //            i *= i;
 //        }
 
-
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz " + i);
+            }
+            if (i % 5 == 0) {
+                System.out.println("Buzz " + i);
+            }
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz " + i);
+            }
+        }
     }
 }
