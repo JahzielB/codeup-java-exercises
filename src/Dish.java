5bbf396d0a37ae5da5c4c5634d16823b1b158391
JahzielB <jahzielb@gmail.com>
2020-06-09 17:11:40 -0500

    finished second mini exercise for oop lecture

--- src/Dish.java
+++ src/Dish.java
@@ -1,10 +1,37 @@
 public class Dish {
 
-    public int costInCents;
-    public String nameOfDish;
-    public boolean wouldRecommend;
+    private int costInCents;
+    public int getCostInCents () {
+        return this.costInCents;
+    }
+    public void setCostInCents (int cost) {
+        this.costInCents = cost;
+    }
+
+    private String nameOfDish;
+    public String getNameOfDish () {
+        return this.nameOfDish;
+    }
+    public void setNameOfDish (String name) {
+        this.nameOfDish = name;
+    }
+
+    private boolean wouldRecommend;
+    public boolean isWouldRecommend () {
+        return this.wouldRecommend;
+    }
+    public void setWouldRecommend (boolean recommend) {
+        this.wouldRecommend = recommend;
+    }
+
+    public Dish(int costInCents, String nameOfDish, boolean wouldRecommend) {
+        this.costInCents = costInCents;
+        this.nameOfDish = nameOfDish;
+        this.wouldRecommend = wouldRecommend;
+    }
 
     public void printSummary () {
-        System.out.printf("Cost: %d\nName: %s\nRecommended: %b", costInCents, nameOfDish, wouldRecommend);
+        System.out.printf("Cost: %d\nName: %s\nRecommended: %b", this.costInCents, this.nameOfDish, this.wouldRecommend);
     }
+
 }
--- src/DishTest.java
+++ src/DishTest.java
@@ -1,12 +1,15 @@
 public class DishTest {
     public static void main(String[] args) {
+//        Dish dish1 = new Dish();
+//        dish1.setCostInCents(4);
+//        dish1.setNameOfDish("pizza");
+//        dish1.setWouldRecommend(true);
+//        dish1.printSummary();
 
-        Dish dish = new Dish();
-
-        dish.costInCents = 350;
-        dish.nameOfDish = "Loch Ness Monster";
-        dish.wouldRecommend = true;
-
+        Dish dish = new Dish(52, "pizza", true);
         dish.printSummary();
+        DishTools.analyzeDishCost(dish);
+        DishTools.shoutDishName(dish);
+        DishTools.flipRecommendation(dish);
     }
 }
