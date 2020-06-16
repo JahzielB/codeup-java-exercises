efffade681620b25a6739a686fff308ec249fd2f
JahzielB <jahzielb@gmail.com>
2020-06-16 09:42:19 -0500

    completed inheritance and polymorphism exercise

---
+++ src/shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square extends Rectangle{
+
+    public Square(int side) {
+        super(side, side);
+    }
+
+    @Override
+    public int getArea() {
+        return super.getLength() * super.getLength();
+    }
+
+    @Override
+    public int getPerimeter() {
+        return 4 * super.getLength();
+    }
+}
